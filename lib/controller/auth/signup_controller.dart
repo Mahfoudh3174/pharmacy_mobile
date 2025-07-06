5b862ff1973f7c5c5645e66ae6a0b39b4dee20cc
Mahfoudh3174 <mahfoudhiscae@gmail.com>
2025-07-06 23:31:33 +0000

    fix

--- lib/controller/auth/signup_controller.dart
+++ lib/controller/auth/signup_controller.dart
@@ -59,7 +59,7 @@ class SignUpControllerImp extends SignUpController {
             toastLength: Toast.LENGTH_LONG,
             gravity: ToastGravity.BOTTOM,
             timeInSecForIosWeb: 1,
-            backgroundColor: Colors.green,
+            backgroundColor: Colors.red,
             textColor: Colors.white,
             fontSize: 16.0,
           );
